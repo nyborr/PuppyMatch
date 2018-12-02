bfa4801ad56cf5cb1a99735e490af54a90080e64
DESKTOP-2P4C3GV\robyn <robynlynnyates@hotmail.com>
2018-12-01 18:25:40 -0600

    Adding to the list of matches activity

---
+++ app/src/main/java/DogProfileActivity.java
@@ -0,0 +1,10 @@
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class DogProfileActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/edu/apsu/csci/puppymatch/MatchesActivity.java
+++ app/src/main/java/edu/apsu/csci/puppymatch/MatchesActivity.java
@@ -5,7 +5,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import java.util.List;
 
 public class MatchesActivity extends ListActivity {
@@ -26,15 +34,53 @@ public class MatchesActivity extends ListActivity {
             Log.i("LISTINGITEMS: ", animal.getName());
         }
 
-//
-//
-//        ListView listView = findViewById(R.id.mylistview);
-//        ArrayAdapter<Animal> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
-//
-//        setListAdapter(adapter);
+       // ArrayAdapter<Animal> adapter = new myAdapter(this, list);
+       // setListAdapter(adapter);
+
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        Animal animal = (Animal) l.getItemAtPosition(position);
+        String gender = animal.getSpecies();
+
+        //Intent intent = new Intent(getApplicationContext(), StateActivity.class);
+        // If serializable dont do this
+//        intent.putExtra(NAME_KEY, state.getName());
+//        intent.putExtra(ABBV_KEY, state.getAbbrv());
+
+        //intent.putExtra(STATE_KEY, state);
+
+
+        //startActivity(intent);
 
     }
 
 
+    class myAdapter extends ArrayAdapter<Animal> {
+
+        private final Animal[] animals;
+
+        public myAdapter(Context context, Animal[] animals) {
+            super(context, R.layout.row, animals);
+            this.animals = animals;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View rowView = layoutInflater.inflate(R.layout.row, parent, false);
+
+            TextView nameTV = rowView.findViewById(R.id.animal_name_textview);
+            nameTV.setText(animals[position].getName());
+            TextView speciesTV = rowView.findViewById(R.id.gender_textView);
+            speciesTV.setText(animals[position].getSpecies());
+
+            return rowView;
+        }
+    }
 
 }
